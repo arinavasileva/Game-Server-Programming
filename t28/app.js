953eeacfae26cd64ecfca989aff12d6635b0690d
arinavasileva <arina.vasileva.ed@gmail.com>
2024-08-28 11:14:29 +0300

    update task28

--- t28/app.js
+++ t28/app.js
@@ -1,14 +1,14 @@
 var http = require('http');
 var fs = require('fs');
 
-http.createServer(function(req, res){
-    
-    res.writeHead(200,{'Content-type':'text/html'});
+http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'application/json'});
     var person = {
         firstname: 'Arina',
         lastname: 'Vasileva'
-        };
+    };
 
-        res.end(JSON.stringify(person));
-
-    }
+    res.end(JSON.stringify(person));
+}).listen(3000, function() {
+    console.log('Server is running on port 3000');
+});
